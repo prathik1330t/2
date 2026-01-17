f9eda402fd6c6c6be42f90089cb8273483283c97
prathik1330t <01fe24bca234@kletech.ac.in>
2026-01-17 16:03:00 +0530

    Create test_2.py

---
+++ test_2.py
@@ -0,0 +1,10 @@
+from 2 import arithmetic_operations
+
+def test_positive_numbers():
+    assert arithmetic_operations(10, 5) == (15, 5, 50, 2.0)
+
+def test_with_zero():
+    assert arithmetic_operations(5, 1) == (6, 4, 5, 5.0)
+
+def test_negative_numbers():
+    assert arithmetic_operations(-10, -5) == (-15, -5, 50, 2.0)

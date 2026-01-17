eb3d1be5f453a709ba403bedda3447b4ed3ea926
prathik1330t <01fe24bca234@kletech.ac.in>
2026-01-17 16:10:03 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,7 +5,7 @@ def arithmetic_operations(num1, num2):
     div = num1 / num2
     return add, sub, mul, div
 
-if _name_ == "_main_":
+if __name__== "__main__":
     num1 = 10
     num2 = 5
     print(arithmetic_operations(num1, num2))
